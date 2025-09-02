2674ad8ddfd62079b049138de58e57fbaa2ccefd
Tuvix Shih <tuvix@ninthday.info>
2025-09-02 22:00:17 +0800

    feat(analyze): 新增 BTM 主題模型分析功能並修正資料夾路徑

--- analyze.py
+++ analyze.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import pandas as pd
 
+from btm import btm_analysis
 from centrality_score import centralityScore
 from floodfire.common.logging import AppLogger
 from floodfire.store.analyze_task import AnalyzeTaskStore
@@ -16,7 +17,7 @@ def centrality_analysis(task):
     Stance = pd.DataFrame()
     raw_predict_data = pd.DataFrame()
 
-    files = [f.name for f in Path(data_folder).iterdir() if f.is_file()]
+    files = [f.name for f in Path(task_data_folder).iterdir() if f.is_file()]
 
     for file in files:
         # 找出開頭是 2 且結尾是 .csv 的檔案
@@ -28,8 +29,11 @@ def centrality_analysis(task):
             score_csv_path = centrality_folder.joinpath(f"{formatted_date}.csv")
 
             if not score_csv_path.exists():
+                # 讀取合併後的週資料
+                combined_dataset = pd.read_csv(task_data_folder.joinpath(file))
+
                 (result, filtered_dataset) = calc_centrality_scores(
-                    file, score_csv_path
+                    file, score_csv_path, combined_dataset
                 )
                 print(result)
                 # 建立不同時間點的立場 DataFrame
@@ -45,32 +49,37 @@ def centrality_analysis(task):
                     Stance = temp_df
                 else:
                     Stance = pd.merge(Stance, temp_df, on=["user", "name"], how="outer")
+                # btm 主題模型分析並儲存
+                calc_btm_topics(file, combined_dataset)
 
 
-def calc_centrality_scores(file, score_csv_path):
-    # 讀取合併後的週資料
-    combined_dataset = pd.read_csv(data_folder.joinpath(file))
-    (
-        score,
-        network_degree,
-        network_betweenness,
-        network_closeness,
-        network_eigenvector,
-        result,
-        filtered_dataset,
-    ) = centralityScore(combined_dataset, score_csv_path)
-    # 儲存中心性分數 CSV
-    score.to_csv(score_csv_path, index=False)
-    # 取得檔案名稱（不含副檔名）作為日期格式
-    formatted_date = file.split(".")[0]
-    # 儲存各種中心性指標檔案
-    save_centrality(
-        formatted_date,
-        network_degree,
-        network_betweenness,
-        network_closeness,
-        network_eigenvector,
-    )
+def calc_centrality_scores(file, score_csv_path, combined_dataset):
+    logger.info(f"Calculating centrality scores for {file}")
+    try:
+        (
+            score,
+            network_degree,
+            network_betweenness,
+            network_closeness,
+            network_eigenvector,
+            result,
+            filtered_dataset,
+        ) = centralityScore(combined_dataset, score_csv_path)
+        # 儲存中心性分數 CSV
+        score.to_csv(score_csv_path, index=False)
+        # 取得檔案名稱（不含副檔名）作為日期格式
+        formatted_date = file.split(".")[0]
+        # 儲存各種中心性指標檔案
+        save_centrality(
+            formatted_date,
+            network_degree,
+            network_betweenness,
+            network_closeness,
+            network_eigenvector,
+        )
+        logger.info(f"Centrality scores calculated and saved for {file}")
+    except Exception as e:
+        logger.error(f"Error calculating centrality scores: {e}")
     return result, filtered_dataset
 
 
@@ -112,8 +121,47 @@ def save_centrality(
         logger.error(f"Error saving centrality JSON files: {e}")
 
 
-def calc_btm_topics(task):
-    pass
+def calc_btm_topics(file, combined_dataset):
+    logger.info(f"Calculating BTM topics for {file}")
+    try:
+        # 取得檔案名稱（不含副檔名）作為日期格式
+        formatted_date = file.split(".")[0]
+        topics_coords_csv_path = topics_coords_folder.joinpath(
+            f"{formatted_date}_topics_coords.csv"
+        )
+        dict_file_path = task_data_folder.joinpath("dictionary.txt")
+        if not topics_coords_csv_path.exists():
+            if not dict_file_path.exists():
+                # 沒有字典檔案的 BTM 分析
+                topics_coords, terms_probs, top_5_doc = btm_analysis(combined_dataset)
+            else:
+                # 有字典檔案的 BTM 分析
+                topics_coords, terms_probs, top_5_doc = btm_analysis(
+                    combined_dataset, dict_file_path
+                )
+
+            # 儲存 BTM 分析結果
+            topics_coords.to_csv(topics_coords_csv_path, index=False)
+            logger.info(f"BTM topics coords saved: {topics_coords_csv_path}")
+            # 儲存各主題詞機率
+            for topic, df in terms_probs.items():
+                terms_probs_csv_path = terms_probs_folder.joinpath(
+                    f"{formatted_date}_{topic}.csv"
+                )
+                df.to_csv(terms_probs_csv_path, index=False)
+                logger.info(f"Terms probabilities saved: {terms_probs_csv_path}")
+
+            # 儲存各主題前五篇文件
+            for topic, df in top_5_doc.items():
+                top_5_docs_csv_path = top_5_docs_folder.joinpath(
+                    f"{formatted_date}_{topic}.csv"
+                )
+                df.to_csv(top_5_docs_csv_path, index=False)
+                logger.info(f"Top 5 documents saved: {top_5_docs_csv_path}")
+        logger.info(f"BTM topics calculated and saved for {file}")
+    except Exception as e:
+        logger.error(f"Error in calculating BTM analysis: {e}")
+        return
 
 
 if __name__ == "__main__":
@@ -137,32 +185,32 @@ if __name__ == "__main__":
     # 建立分析所需的資料夾
     logger.info("Creating analysis folders...")
     # Create the main data folder if it doesn't exist
-    data_folder = Path("data/{}".format(task["name"]))
-    if not data_folder.exists():
-        data_folder.mkdir(parents=True)
+    task_data_folder = Path("data/{}".format(task["name"]))
+    if not task_data_folder.exists():
+        task_data_folder.mkdir(parents=True)
 
     # Create centrality folder if it doesn't exist
-    centrality_folder = data_folder.joinpath("centrality")
+    centrality_folder = task_data_folder.joinpath("centrality")
     if not centrality_folder.exists():
         centrality_folder.mkdir(parents=True, exist_ok=True)
 
     # Create topics_coords folder if it doesn't exist
-    topics_coords_folder = data_folder.joinpath("btm", "topics_coords")
+    topics_coords_folder = task_data_folder.joinpath("btm", "topics_coords")
     if not topics_coords_folder.exists():
         topics_coords_folder.mkdir(parents=True, exist_ok=True)
 
     # Create terms_probs folder if it doesn't exist
-    terms_probs_folder = data_folder.joinpath("btm", "terms_probs")
+    terms_probs_folder = task_data_folder.joinpath("btm", "terms_probs")
     if not terms_probs_folder.exists():
         terms_probs_folder.mkdir(parents=True, exist_ok=True)
 
     # Create top_5_docs folder if it doesn't exist
-    top_5_docs_folder = data_folder.joinpath("btm", "top_5_docs")
+    top_5_docs_folder = task_data_folder.joinpath("btm", "top_5_docs")
     if not top_5_docs_folder.exists():
         top_5_docs_folder.mkdir(parents=True, exist_ok=True)
 
     # Create network folder if it doesn't exist
-    network_folder = data_folder.joinpath("network")
+    network_folder = task_data_folder.joinpath("network")
     if not network_folder.exists():
         network_folder.mkdir(parents=True, exist_ok=True)
     centrality_analysis(task)
